fb1aa3ebce8a432e41ffe5ec7c3e1d94c34d17b9
Keshav Abhishek <crystaled2003@gmail.com>
2023-08-21 23:17:03 +0530

    Upload

--- LAB_ASSIGNMENT/WEEK-2/Q3.c
+++ LAB_ASSIGNMENT/WEEK-2/Q3.c
@@ -4,6 +4,7 @@
 
 int main(){
     int rows, columns;
+    char labels[3][100]={"Rows    :   ", "Columns :   ", "Value   :   "};
     printf("Enter rows and columns (rxc): ");
     scanf("%dx%d", &rows, &columns);
 
@@ -27,22 +28,37 @@ int main(){
         }
 
     }
+    int sparse[3][countNon0];
+    int iterator=0;
     printf("\n\n");
-    if(count0<countNon0){
-        printf("Not a sparse matrix.");
-    }
-    else{
-        printf("i\tj\tValue\n\n");
+    if(count0>=(rows*columns)/100){
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
                 if(array[i][j]!=0){
-                    printf("(%d\t%d\t%d)\n", i, j, array[i][j]);
+                    sparse[0][iterator]=i;
+                    sparse[1][iterator]=j;
+                    sparse[2][iterator]=array[i][j];
+                    iterator++;
                 }
             }
             
         }
+
+        for (int i = 0; i < 3; i++)
+        {
+            printf("%s", labels[i]);
+            for (int j = 0; j < countNon0; j++)
+            {
+                printf("%d ", sparse[i][j]);
+            }
+            printf("\n");
+            
+        }
+    }
+    else{
+        printf("Not a sparse matrix.");
     }
     return 0;
 }
